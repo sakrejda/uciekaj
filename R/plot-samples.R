b5e846460d4f5771b1303df0886d3bd6427b678a
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2014-07-16 03:38:56 +0000

    Debugged plot-samples function.

--- R/plot-samples.R
+++ R/plot-samples.R
@@ -5,10 +5,11 @@ plot_sample_trace <- function(sample_matrix, type=geom_line()) {
 	pl <- pl + type
 	cnr <- cn[!(cn %in% c('chain','iteration','value'))]
 	n_levels <- sapply(cnr, function(x) length(unique(sample_melt[[x]])))
-	max_levels <- names(n_levels)[n_levels == max(n_levels)]
-	other_levels <- names(n_levels)[n_levels != max(n_levels)]
+	max_levels <- names(n_levels)[n_levels == max(n_levels)][1]
+	other_levels <- names(n_levels)[names(n_levels) != max_levels]
 	pl <- pl + facet_grid(
-		facets = paste0(max_levels, "~", paste(other_levels, collapse='+'))
+		facets = paste0(max_levels, "~", paste(other_levels, collapse='+'),
+		scales='free_y')
 	)
 	return(pl)
 }
